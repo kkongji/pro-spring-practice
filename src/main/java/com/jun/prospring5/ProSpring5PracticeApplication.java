4ff75d40bfdd433c1bdd591da7188d0edcb6023f
junyoung.han <hjy0085@naver.com>
2021-12-22 11:27:35 +0900

    initialize

---
+++ src/main/java/com/jun/prospring5/ProSpring5PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jun.prospring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSpring5PracticeApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ProSpring5PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jun/prospring5/ProSpring5PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.prospring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSpring5PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
